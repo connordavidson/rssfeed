6abf3a6646d6d12d166248c6a26aad661a98814b
John Doe <connor.j.davidson@gmail.com>
2019-06-14 16:39:27 -0400

    first commit

---
+++ rssfeed.py
@@ -0,0 +1,2 @@
+#Given a Dictionary keyed by Company and valued by RSS feed url, write a function that determines which companies had no activity for a given number of days
+#by Connor Davidson. created June 14,2019.
