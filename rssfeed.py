3e72c8421541b615fbe7ce6ab739da1367fbf2bf
John Doe <connor.j.davidson@gmail.com>
2019-06-17 20:11:28 -0400

    Updated the function to compare the current time and time of the entries updates.

--- rssfeed.py
+++ rssfeed.py
@@ -3,31 +3,20 @@
 
 
 
-
-#might need recursion for the "some companies might have more than one feed" part. some companies just provide links to a page with several rss pages.
-#get the parsed rss info from the URL, then iterate through it's info (something like: while x < len(entries)) and use .updated.
-#if it hasn't been updated within the given number of days, then the company name gets put into an array. 
-
-
-#might need to get the xml file and go through it looking for <link> tags..
-#   get their contents and do a contains(.rss) on it to check if it has it in it 
-
-
-
-
+#used to parse rss feeds
 import feedparser
+#used to verify that ssl is working currectly
 import ssl
+#used for doing time math
+from datetime import datetime
+from dateutil.parser import parse
 
 
+#assumtion:  the dictionary is similar to this one .. where every url links to a RSS feed. if not, can check if it is an xml file or not 
 feeds = {
-        "bbc1"       : "http://feeds.bbci.co.uk/news/rss.xml",
-        "bbc2"       : "http://feeds.bbci.co.uk/news/world/rss.xml",
-        "bbc3"       : "http://feeds.bbci.co.uk/news/education/rss.xml",
+        "hackerNews" : "https://hnrss.org/newest",
         "craigslist" : "https://www.craigslist.org/about/best/all/index.rss",
         "Joe Rogan"  : "http://podcasts.joerogan.net/feed",
-        "espn1"      : "https://www.espn.com/espn/rss/news",
-        "espn2"      : "https://www.espn.com/espn/rss/nfl/news",
-        "espn3"      : "https://www.espn.com/espn/rss/espnu/news",
         "reddit1"    : "https://www.reddit.com/.rss",
         "reddit2"    : "https://www.reddit.com/r/news/.rss",
         "WSJ"        : "https://feeds.a.dj.com/rss/RSSWorldNews.xml",
@@ -36,24 +25,42 @@ feeds = {
          }
 
 
-
-
 #makes sure ssl is working correctly
 if hasattr(ssl, '_create_unverified_context'):
     ssl._create_default_https_context = ssl._create_unverified_context
 
 
-#this 
-for company, url in feeds.items():
-    d = feedparser.parse(url)
-    print(company.upper() )
-    print(len(d.entries))
-    for entry in range(0, len(d.entries)):
-        print( str(entry) + " : " +d.entries[entry].title)
-        print( d.entries[entry].updated )
+def rss_activity(feeds , days ):
+    
+    today = datetime.now()
+
+    #prints the number of days.. casts the int to a string.
+    print(str(days) + " days")
+
+    # loops through the items in the feeds dict and prints the company name in all caps and then loops through the entries in the given url
+    for company, url in feeds.items():
+        d = feedparser.parse(url)
+        print( company.upper() )
+        
+        #loops through all of the entries on the rss feed
+        for entry in range(0, len(d.entries)):
+            #stores the updated time into a string
+            entryUpdatedTime = d.entries[entry].updated
+            #trims the last 6 characters off of the given entries time so that it can use strptime(). this contained the time zone.
+            trimmedEntryUpdatedTime = entryUpdatedTime[:-6]
+            
+            #converts the trimmedEntryUpdatedTime into a datetime object for the subtraction
+            #assumption: the time to be parsed contains every element that is necessary (year, month, days, hours, minutes, seconds, etc) 
+            realTime = parse(trimmedEntryUpdatedTime)
+            #checks if the updated time is more or less than the days parameter
+            if (today - realTime).days >= days:
+                print("more")
+               
         print()
-    print()
-    print()
+        print()
+
+
+rss_activity(feeds, 5)
 
 
 
